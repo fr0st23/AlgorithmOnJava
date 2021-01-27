156c0e5322942c82cfe2dae824d244f3b9d325ac
Nikita <nickita67@gmail.com>
2021-01-27 16:44:45 +0300

    Update Main class

--- src/org/nicki/Main.java
+++ src/org/nicki/Main.java
@@ -8,12 +8,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        new Main().sortCompare();
+    }
+
+    private void sortCompare(){
         Scanner scanner = new Scanner(System.in);
 
-        String alg1 = scanner.nextLine();
-        String alg2 = scanner.nextLine();
+        System.out.println(
+                "Бвыберите способ сортировки для сравнения\n" +
+                        "1) Вставки\n" +
+                        "2) Выбор\n" +
+                        "3) Шелла\n" +
+                        "4) Слияние\n" +
+                        "5) Быстрая"
+        );
+        String alg1 = choise(scanner.nextInt());
+        System.out.print("С каким алгоритмом сравнить: ");
+        String alg2 = choise(scanner.nextInt());
 
+        System.out.print("Кол-во элементов в массиве: ");
         int n = scanner.nextInt();
+        System.out.print("Кол-во массивов: ");
         int t = scanner.nextInt();
 
         double t1 = SortCompare.timeRandomInput(alg1, n, t);
@@ -21,7 +36,28 @@ public class Main {
 
         StdOut.printf("Для %d случайных целых чисел\n %s в", n, alg1);
         StdOut.printf(" %.1f раз быстрее, чем %s\n", t2/t1, alg2);
+    }
 
+    private String choise(int tmp){
+        String str = "";
+        switch (tmp){
+            case 1:
+                str = "Вставки";
+                break;
+            case 2:
+                str = "Выбор";
+                break;
+            case 3:
+                str = "Шелла";
+                break;
+            case 4:
+                str = "Слияние";
+                break;
+            case 5:
+                str = "Быстрая";
+                break;
+        }
+        return str;
     }
 }
 
